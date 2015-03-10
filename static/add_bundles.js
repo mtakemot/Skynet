c3bbbe812cf1fabf2a125e4ad1fe3984f15a8ae1
chc_roeder <chc217@ucsd.edu>
2015-03-09 20:10:44 -0700

    Updated UI

--- static/add_bundles.js
+++ static/add_bundles.js
@@ -1,3 +1,26 @@
 /**
  * Created by roeder on 3/9/15.
  */
+
+var main = function(){
+
+   $('.icon-menu').click( function(){
+       $('.menu').animate({
+           right: '0px'
+           }, 200);
+    $('body').animate({
+        right: '285px'
+    }, 200);
+   });
+
+   $('.icon-close').click( function(){
+        $('.menu').animate({
+            right: '-285px'
+            }, 200);
+        $('body').animate({
+            right: '0px'
+        }, 200);
+   });
+};
+
+$(document).ready(main);

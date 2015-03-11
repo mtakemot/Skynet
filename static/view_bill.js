db0a04e9fc4954492be19ee7ab1e567047b1f1be
chc_roeder <chc217@ucsd.edu>
2015-03-11 03:39:22 -0700

    Modified UI

--- static/view_bill.js
+++ static/view_bill.js
@@ -1,6 +1,7 @@
 /**
  * Created by roeder on 3/8/15.
  */
+     var current_position1 = 0;
 var main = function(){
 
    $('.icon-menu').click( function(){
@@ -10,6 +11,7 @@ var main = function(){
     $('body').animate({
         right: '285px'
     }, 200);
+       current_position1 = 285;
    });
 
    $('.icon-close').click( function(){
@@ -19,6 +21,18 @@ var main = function(){
         $('body').animate({
             right: '0px'
         }, 200);
+   });
+
+    $('.icon-menu').dblclick( function(){
+           if( current_position1 === 285 ) {
+               $('.menu').animate({
+                   right: '-285px'
+               }, 200);
+               $('body').animate({
+                   right: '0px'
+               }, 200);
+               current_position1 = 0;
+           }
    });
 };
 

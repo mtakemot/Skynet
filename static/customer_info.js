d09a3fed46caaf2d27c0766270fd4a46a371b47a
chc_roeder <chc217@ucsd.edu>
2015-03-11 13:57:25 -0700

    Modifing UI

---
+++ static/customer_info.js
@@ -0,0 +1,39 @@
+/**
+ * Created by roeder on 3/11/15.
+ */
+     var current_position1 = 0;
+var main = function(){
+
+   $('.icon-menu').click( function(){
+       $('.menu').animate({
+           right: '0px'
+           }, 200);
+    $('body').animate({
+        right: '285px'
+    }, 200);
+       current_position1 = 285;
+   });
+
+   $('.icon-close').click( function(){
+        $('.menu').animate({
+            right: '-285px'
+            }, 200);
+        $('body').animate({
+            right: '0px'
+        }, 200);
+   });
+
+    $('.icon-menu').dblclick( function(){
+           if( current_position1 === 285 ) {
+               $('.menu').animate({
+                   right: '-285px'
+               }, 200);
+               $('body').animate({
+                   right: '0px'
+               }, 200);
+               current_position1 = 0;
+           }
+   });
+};
+
+$(document).ready(main);
